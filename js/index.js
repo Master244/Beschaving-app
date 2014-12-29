f2b51f61cac4cbe76a07af65f598564e17061910
Mitchell Tieleman <mitchell@netwerven.nl>
2014-12-29 01:34:57 +0100

    update

--- js/index.js
+++ js/index.js
@@ -294,3 +294,24 @@ var app = {
         // save the to-do list
         saveToDoList();
     }
+
+    // Google Maps Function
+    function GoogleMap(){
+     
+        this.initialize = function(){
+        var map = showMap();
+        }
+         
+         var showMap = function(){
+        var mapOptions = {
+        zoom: 4,
+        center: new google.maps.LatLng(-33, 151),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+         
+        var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+         
+        return map;
+        }
+    }
+
